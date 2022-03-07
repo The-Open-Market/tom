47e05f39564b904e31a8521b9196797fbf64a04f
Nick <nicktehrany1@gmail.com>
2022-03-07 20:22:57 +0100

    Added accepting and rejecting of orders to buttons.
    Can now click the buttons to accept and reject the orders. Also fixed
    the issue of having duplicate orders listed because of the event
    listener.
    TODO: we still want to distinguish between pending and accepted orders
    in separate lists, and refactor the complex functions.

--- client/src/components/OrderTable.vue
+++ client/src/components/OrderTable.vue
@@ -69,23 +69,14 @@
                 >
                   <tr v-for="order in orders" :key="order.id">
                     <td class="px-6 py-4 whitespace-nowrap">
-                      <!--<div class="flex items-center">
-                                <div class="flex-shrink-0 h-10 w-10">
-                                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
-                                </div>
-                                <div class="ml-4">
-                                    <div class="text-sm font-medium text-gray-900">Jane Cooper</div>
-                                    <div class="text-sm text-gray-500">jane.cooper@example.com</div>
-                                </div>
-                                </div>-->
                       {{ order.id }}
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">xxx</td>
                     <td class="px-6 py-4 whitespace-nowrap">
                       <div class="text-sm text-gray-900">
-                        Regional Paradigm Technician
+                        PLACEHOLDER
                       </div>
-                      <div class="text-sm text-gray-500">Optimization</div>
+                      <div class="text-sm text-gray-500">TNO</div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
                       <span
@@ -99,7 +90,7 @@
                       <button
                         type="button"
                         class="text-green-700 hover:text-white transition-colors duration-200 border border-green-700 hover:bg-gren-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
-                      >
+                      @click="approveOrder(order.id)">
                         Accept
                       </button>
                     </td>
@@ -109,13 +100,13 @@
                       <button
                         type="button"
                         class="text-red-700 hover:text-white transition-colors duration-200 border border-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-800"
-                      >
+                      @click="rejectOrder(order.id)">
                         Reject
                       </button>
                     </td>
                   </tr>
                 </tbody>
-                <tbody velse>
+                <tbody v-else>
                   <td>No entries yet</td>
                 </tbody>
               </table>
@@ -130,9 +121,28 @@
 <script>
 export default {
   name: "OrderTable",
+
+  emits: ["approveOrder", "rejectOrder"],
+
   props: {
     orders: [],
   },
+
+  setup(_, context) {
+
+    const approveOrder = (order) => {
+      context.emit("approveOrder", order);
+    };
+
+    const rejectOrder = (order) => {
+      context.emit("rejectOrder", order);
+    };
+
+    return {
+        approveOrder,
+        rejectOrder
+    }
+  }
  
 };
 </script>
--- client/src/views/Seller.vue
+++ client/src/views/Seller.vue
@@ -1,5 +1,5 @@
 <template>
-    <OrderTable :orders="this.pendingOrders" />
+    <OrderTable :orders="this.pendingOrders" @approveOrder="approveOrder" @rejectOrder="rejectOrder"/>
 </template>
 
 <script>
@@ -12,6 +12,7 @@ export default {
 
     setup() {
         const pendingOrders = reactive([])
+        const approvedOrders = reactive([])
         const sellerAddress = "0x59Ce492d182688239C118AcFEb1A4872Ce3B1231";    
 
         const approveOrder = async(orderId) => {
@@ -19,12 +20,24 @@ export default {
             await tnoEats.approveOrder(orderId);
             console.log("Seller " + sellerAddress + " approved order " + orderId); 
 
+            // TODO: Figure out way of having pending and approved arrays
             pendingOrders.find(order=>order.id===orderId).status = "Approved";
+        }
+
+        const rejectOrder = async(orderId) => {
+            const { tnoEats } = await getSmartContract();
+            await tnoEats.rejectOrder(orderId);
+            console.log("Seller " + sellerAddress + " rejected order " + orderId); 
+
+            // TODO: Maybe remove the rejected orders from view?
+            pendingOrders.find(order=>order.id===orderId).status = "Rejected";
         }
 
         return {
             pendingOrders,
-            approveOrder
+            approvedOrders,
+            approveOrder,
+            rejectOrder
         }
     },
 
@@ -32,40 +45,47 @@ export default {
         OrderTable
     },
     
-    // TODO: IPFS integration
-    // TODO: with OrderPlaced query for the orderid and get the orderInfo
-    // TODO: add listener to get newly placed orders so that we don't recheck entire chain from block 0 and need to refresh everytime
     async mounted() {
         const { tnoEats } = await getSmartContract();
         const sellerAddress = "0x59Ce492d182688239C118AcFEb1A4872Ce3B1231";    
-
         let orders = await tnoEats.getOrdersBySeller(sellerAddress);
-        console.log("Order ids: " + orders);
-        
-        const events = await tnoEats.queryFilter("OrderApproved", 0); // On mount filter from block 0
+
+        const OrderApprovedEvents = await tnoEats.queryFilter("OrderApproved", 0); // On mount filter from block 0
+        const OrderPlacedEvents = await tnoEats.queryFilter("OrderPlaced", 0);
         tnoEats.on('OrderPlaced', (seller, orderId, ipfsUrl) => {
-            console.log(seller)
-            console.log(ipfsUrl)
-            this.pendingOrders.push({
-                id: orderId
-            })
+            console.log("Order Placed event listener " + orderId);
+            if(!this.pendingOrders.some(pendingOrder => pendingOrder.id === parseInt(orderId._hex, 16))) {
+                this.pendingOrders.push({
+                    id: parseInt(orderId._hex, 16),
+                    status: "Pending",
+                    ipfsUrl: ipfsUrl
+                })
+            }
         })
 
+        // TODO: Refactor this, probably a better way of doing this
         for(var order in orders){
             let approved = false;
+            let ipfsUrl = "PLACEHOLDER";
 
-            // Check if orderId exists in events
-            for(const event of events) {
+            // Check if orderId exists in OrderApprovedOrderApprovedEvents
+            for(const event of OrderApprovedEvents) {
                if(parseInt(event.args.orderId._hex, 16) === parseInt(order)) {
                     approved = true;
                 } 
             }
+              // Get the ipfsURL from the placedOrderEvents
+            for(const event of OrderPlacedEvents) {
+               if(parseInt(event.args.orderId._hex, 16) === parseInt(order)) {
+                    ipfsUrl = event.args.orderContentsUrl;
+                } 
+            }
 
-            if(!approved) {
-                this.pendingOrders.push({id: order, status: "Pending"});
-            } else {
+            if(!approved && !this.pendingOrders.some(pendingOrder => pendingOrder.id === parseInt(order))) {
+                this.pendingOrders.push({id: parseInt(order), status: "Pending", ipfsUrl: ipfsUrl});
+            } else if (!this.pendingOrders.some(pendingOrder => pendingOrder.id === parseInt(order))) {
                 console.log("Order " + order + " already Approved.");
-                this.pendingOrders.push({id: order, status: "Approved"});
+                this.pendingOrders.push({id: parseInt(order), status: "Approved", ipfsUrl: ipfsUrl});
             }
         }
     }
