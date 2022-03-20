9baec7308aa032be76238c2f81e4263dd121e74a
Nick <nicktehrany1@gmail.com>
2022-03-20 19:43:19 +0100

    fix migration

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -52,21 +52,23 @@ async function addDemoData(eurtContract, tnoEatsContract, [client, seller, deliv
 }
 
 module.exports = async (deployer, network) => {
-    if(network == "geth") {
+    if(network === "geth") {
         const [deployAddress, client, seller, deliveryService, _client, _seller, _deliveryService] = await web3.eth.getAccounts();
-
-        let oneEurt = new BigNumber('1000000000000000000');
-        oneEurt.shiftedBy(-18);
 
         await deployer.deploy(EurTno, oneEurt.multipliedBy(6000000));
         const eurtContract = await EurTno.deployed();
+
         await deployer.deploy(TnoEats, eurtContract.address);
+
         await eurtContract.transfer(client, oneEurt.multipliedBy(1000000), { from: deployAddress });
         await eurtContract.transfer(seller, oneEurt.multipliedBy(1000000), { from: deployAddress });
         await eurtContract.transfer(deliveryService, oneEurt.multipliedBy(1000000), { from: deployAddress });
         await eurtContract.transfer(_client, oneEurt.multipliedBy(1000000), { from: deployAddress });
         await eurtContract.transfer(_seller, oneEurt.multipliedBy(1000000), { from: deployAddress });
         await eurtContract.transfer(_deliveryService, oneEurt.multipliedBy(1000000), { from: deployAddress });
+        await addDemoData(eurtContract, tnoEatsContract, [client, seller, deliveryService, _client, _seller, _deliveryService]);
+
+        console.log(`EURT: ${eurtContract.address}`);
     } else {
         const [client, seller, deliveryService, _client, _seller, _deliveryService] = await web3.eth.getAccounts();
 
@@ -78,8 +80,8 @@ module.exports = async (deployer, network) => {
         await eurtContract.transfer(_client, oneEurt.multipliedBy(1000000), { from: client });
         await eurtContract.transfer(_seller, oneEurt.multipliedBy(1000000), { from: client });
         await eurtContract.transfer(_deliveryService, oneEurt.multipliedBy(1000000), { from: client });
-    }
-    await addDemoData(eurtContract, tnoEatsContract, [client, seller, deliveryService, _client, _seller, _deliveryService]);
+        await addDemoData(eurtContract, tnoEatsContract, [client, seller, deliveryService, _client, _seller, _deliveryService]);
 
-    console.log(`EURT: ${eurtContract.address}`);
+        console.log(`EURT: ${eurtContract.address}`);
+    }
 };
