c7b9b9933f817a91008b68a371a4c6c818f07ebc
Nick <nicktehrany1@gmail.com>
2022-03-27 14:15:29 +0200

    Updated deploy script
    Co-authored-by: paolokazemi <paolokazemi@users.noreply.github.com>

--- client/deploy.sh
+++ client/deploy.sh
@@ -9,7 +9,7 @@ NODE_ENV=production npm run build
 cd dist
 cp index.html 404.html
 
-author="$(git log -1 | grep Author | awk '{print $2;exit}')"
+author="$(git log -1 --format='%an' | grep Author | awk '{print $2;exit}')"
 email="$(git log -1 | grep Author | awk '{print $3;exit}' | sed -e 's/[<>]//g')"
 coauthor="$(git log -1 | grep Co-authored-by)"
 
