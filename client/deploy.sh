62b35988b5121ab3e5f78591342699f233bc1a3e
Nick <nicktehrany1@gmail.com>
2022-03-27 13:28:13 +0200

    Updated deploy script
    Co-authored-by: paolokazemi <paolokazemi@users.noreply.github.com>

--- client/deploy.sh
+++ client/deploy.sh
@@ -8,11 +8,15 @@ NODE_ENV=production npm run build
 cd dist
 cp index.html 404.html
 
+author="$(git log | grep Author | awk '{print $2;exit}')"
+email="$(git log | grep Author | awk '{print $3;exit}' | sed -e 's/[<>]//g')"
+coauthor="$(git log -1 | grep -m1 Co-authored-by)"
+
 git init -b gh-pages
-git config user.name "$(git log | grep Author | awk '{print $2;exit}')"
-git config user.email "$(git log | grep Author | awk '{print $3;exit}' | sed -e 's/[<>]//g')"
+git config user.name $author
+git config user.email $email
 git add -A
-git commit -m "page deployment" -m "$(git log -1 | grep -m1 Co-authored-by)"
+git commit -m "page deployment" -m $coauthor
 git remote add origin https://github.com/nicktehrany/tno-eats
 git push -f origin gh-pages
 
