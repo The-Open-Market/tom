43e21f49c22d31dbfaf01f2d98380e20126ac8fb
Nick <nicktehrany1@gmail.com>
2022-03-27 13:24:20 +0200

    Updated workflow for deploying
    Co-authored-by: paolokazemi <paolokazemi@users.noreply.github.com>

--- client/deploy.sh
+++ client/deploy.sh
@@ -9,10 +9,10 @@ cd dist
 cp index.html 404.html
 
 git init -b gh-pages
-git config user.name $((git log | grep Author | awk '{print $2;exit}'))
-git config user.email $((git log | grep Author | awk '{print $3;exit}' | sed -e 's/[<>]//g'))
+git config user.name "$(git log | grep Author | awk '{print $2;exit}')"
+git config user.email "$(git log | grep Author | awk '{print $3;exit}' | sed -e 's/[<>]//g')"
 git add -A
-git commit -m 'page deployment'
+git commit -m "page deployment" -m "$(git log -1 | grep -m1 Co-authored-by)"
 git remote add origin https://github.com/nicktehrany/tno-eats
 git push -f origin gh-pages
 
