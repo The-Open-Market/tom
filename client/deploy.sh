31d352526a28ecdd8f55648c9a4b9887c74f63b9
Nick <nicktehrany1@gmail.com>
2022-03-27 13:03:09 +0200

    Updated workflow
    Co-authored-by: paolokazemi <paolokazemi@users.noreply.github.com>

--- client/deploy.sh
+++ client/deploy.sh
@@ -9,6 +9,8 @@ cd dist
 cp index.html 404.html
 
 git init -b gh-pages
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git add -A
 git commit -m 'page deployment'
 git remote add origin https://github.com/nicktehrany/tno-eats
