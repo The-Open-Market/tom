9ad3b8d8cabb8840f117d8a7b550dcce00ef0e8c
Nick <nicktehrany1@gmail.com>
2022-03-27 14:18:58 +0200

    Updated deploy script
    Co-authored-by: paolokazemi <paolokazemi@users.noreply.github.com>

--- client/deploy.sh
+++ client/deploy.sh
@@ -11,13 +11,12 @@ cp index.html 404.html
 
 author="$(git log -1 --format='%an')"
 email="$(git log -1 --format='%ae')"
-coauthor="$(git log -1 | grep Co-authored-by)"
 
 git init -b gh-pages
 git config user.name "$author"
 git config user.email "$email"
 git add -A
-git commit -m "page deployment" -m "$coauthor"
+git commit -m "page deployment"
 git remote add origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_ACTOR}/${REPOSITORY}.git"
 git push -f origin gh-pages
 
